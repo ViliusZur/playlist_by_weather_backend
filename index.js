20a8ec6f5f29d5c4958fc6b968004686459d0b09
ViliusZur <vilius.zurauskas@gmail.com>
2020-04-30 19:44:54 +0100

    back end receives data from front end

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 "use strict";
 
 var Koa = require("koa");
+const cors = require("@koa/cors");
 
 var app = new Koa();
+app.use(cors());
 
 //import all routes
 var main = require("./routes/main.js");
--- routes/main.js
+++ routes/main.js
@@ -11,17 +11,35 @@ var router = Router({
     prefix: "/main"
 });
 
-router.post("/", async (ctx, next) => {
+router.post("/", bodyParser(), async (ctx, next) => {
 /*
     // first, authenticate spotify app
     await authenticate.authenticateSpotify();
 */
     const body = ctx.request.body;
 
-    // receive mood and danceability from front end
-    const mood = ctx.request.body.values.mood;
-    const danceability = ctx.request.body.values.danceability;
-    console.log(mood, danceability);
+    // receive valence, danceability and weatherID from front end
+    const valence = body.valence;
+    let weatherID = body.weatherID;
+
+    // convert weatherID to energy range
+    const energyRanges = {
+        2: [0, 0.2],
+        3: [0.3, 0.6],
+        5: [0, 0.2],
+        6: [0.1, 0.4],
+        7: [0, 0.3],
+        8: [0.5, 1]
+    };
+    weatherID = (weatherID / 100) | 0;
+    let energy = energyRanges[weatherID];
+
+    // generate random energy level from the given range
+    let min = energy[0];
+    let max = energy[1];
+    energy = (Math.random() * (max - min) + min).toFixed(2);
+
+    console.log(valence + " " + energy);
 /*
     // receive Spotify username from front end, get user's favourite artists
     const username = ctx.request.body.values.username;
