2c4371cd76771d92e7789ba6b3602f84bc562849
ViliusZur <vilius.zurauskas@gmail.com>
2020-05-23 04:44:38 +0100

    fix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var Koa = require("koa");
 const cors = require("@koa/cors");
 var bodyParser = require("koa-bodyparser");
-var DelayedResponse = require("http-delayed-response");
 
 var app = new Koa();
 
@@ -14,14 +13,63 @@ var main = require("./routes/main.js");
 var authoriseSpotify = require("./routes/spotify.js");
 var weather = require("./routes/weather.js");
 
+// function to extend timeouts for spotify requests
+const extendTimeoutMiddleware = (req, res, next) => {
+    const space = ' ';
+    let isFinished = false;
+    let isDataSent = false;
+  
+    // Only extend the timeout for API requests
+    if (!req.url.includes('/Spotify')) {
+      next();
+      return;
+    }
+  
+    res.once('finish', () => {
+      isFinished = true;
+    });
+  
+    res.once('end', () => {
+      isFinished = true;
+    });
+  
+    res.once('close', () => {
+      isFinished = true;
+    });
+  
+    res.on('data', (data) => {
+      // Look for something other than our blank space to indicate that real
+      // data is now being sent back to the client.
+      if (data !== space) {
+        isDataSent = true;
+      }
+    });
+  
+    const waitAndSend = () => {
+      setTimeout(() => {
+        // If the response hasn't finished and hasn't sent any data back....
+        if (!isFinished && !isDataSent) {
+          // Need to write the status code/headers if they haven't been sent yet.
+          if (!res.headersSent) {
+            res.writeHead(202);
+          }
+  
+          res.write(space);
+  
+          // Wait another 15 seconds
+          waitAndSend();
+        }
+      }, 15000);
+    };
+  
+    waitAndSend();
+    next();
+  };
+
+
+app.use(extendTimeoutMiddleware);
 app.use(main.routes());
-app.use(function (req, res) {
-    var delayed = new DelayedResponse(req, res);
-    delayed.start();
-    authoriseSpotify.routes()
-    // will eventually end when the promise is fulfilled
-    delayed.end();
-});
+app.use(authoriseSpotify.routes());
 app.use(weather.routes());
 
 var port = process.env.PORT || 3300;
