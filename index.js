efef240e8dbda57b890b6a990a71d7da5b003a21
ViliusZur <vilius.zurauskas@gmail.com>
2020-05-24 16:56:27 +0100

    fix(index): deleted koa-socket-2

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 "use strict";
 
 var Koa = require("koa");
-const IO = require('koa-socket-2');
 const cors = require("@koa/cors");
 var bodyParser = require("koa-bodyparser");
 
 var app = new Koa();
-const io = new IO();
 
 app.use(cors());
 app.use(bodyParser());
@@ -17,23 +15,6 @@ var authoriseSpotify = require("./routes/spotify.js");
 
 app.use(main.routes());
 app.use(authoriseSpotify.routes());
-
-// this is for opening up a web socket so the client wouldnt time out when generating a playlist
-io.attach(app);
-
-io.on('connection', () => {
-    console.log('Client connected');
-
-    // send current time to the client every 15 seconds
-    setInterval(() => io.emit('time', new Date().toTimeString()), 15000);
-
-    io.on('disconnect', () => console.log('Client disconnected'));
-});
-
-io.on('message', (ctx, data) => {
-    console.log('client sent data to message endpoint', data);
-});
-
 
 var port = process.env.PORT || 3300;
 app.listen(port);
